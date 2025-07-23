3fd1b4d79acb14ad2fa6cfdfc252235464e51aa7
github-action-benchmark <github@users.noreply.github.com>
2025-07-23 18:37:29 +0000

    add VisualVault Benchmark (cargo) benchmark result for 6642b15a2912a8533fc6da5584443c4781b1f75c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753293878104,
+  "lastUpdate": 1753295849844,
   "repoUrl": "https://github.com/mikeleppane/visualvault",
   "entries": {
     "VisualVault Benchmark": [
@@ -161,6 +161,168 @@ window.BENCHMARK_DATA = {
             "name": "scanner_parallel/8",
             "value": 41664000,
             "range": "± 735000",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mleppan23@gmail.com",
+            "name": "Mikko Leppänen",
+            "username": "mikeleppane"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6642b15a2912a8533fc6da5584443c4781b1f75c",
+          "message": "Merge pull request #2 from ccamel/patch-1\n\nfix(docs): update clone URL to point to repo",
+          "timestamp": "2025-07-23T21:26:54+03:00",
+          "tree_id": "7b91c32008dfe278c1d2154f3021b9447436a6d6",
+          "url": "https://github.com/mikeleppane/visualvault/commit/6642b15a2912a8533fc6da5584443c4781b1f75c"
+        },
+        "date": 1753295849422,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "duplicate_detection/1000",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "duplicate_detection/5000",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "duplicate_detection/10000",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "#2",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "#2",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "#2",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "duplicate_ratios/10%",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "duplicate_ratios/30%",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "duplicate_ratios/50%",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "duplicate_ratios/70%",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "organize_by_type/100",
+            "value": 5410900,
+            "range": "± 85700",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "organize_by_type/500",
+            "value": 27611000,
+            "range": "± 469000",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "organize_by_type/1000",
+            "value": 58218000,
+            "range": "± 628000",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "organize_modes/yearly",
+            "value": 59829000,
+            "range": "± 1224000",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "organize_modes/monthly",
+            "value": 60259000,
+            "range": "± 774000",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "organize_modes/type",
+            "value": 57746000,
+            "range": "± 1254000",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "scanner/100",
+            "value": 2695600,
+            "range": "± 37900",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "scanner/1000",
+            "value": 24423000,
+            "range": "± 73000",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "scanner/5000",
+            "value": 124630000,
+            "range": "± 610000",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "scanner_parallel/1",
+            "value": 144720000,
+            "range": "± 1770000",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "scanner_parallel/2",
+            "value": 43990000,
+            "range": "± 169000",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "scanner_parallel/4",
+            "value": 40433000,
+            "range": "± 187000",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "scanner_parallel/8",
+            "value": 37857000,
+            "range": "± 143000",
             "unit": "ns/iter"
           }
         ]
